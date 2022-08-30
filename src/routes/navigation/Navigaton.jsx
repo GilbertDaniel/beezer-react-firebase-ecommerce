fd3ec054145495c0584964f1726106f236488459
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-08-30 16:56:55 +0630

    Implementing Routes and Navigation Component

---
+++ src/routes/navigation/Navigaton.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import './navigation.styles.scss';
+
+import BeezerLogo from '../../assets/beezer.jpg';
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          {/* Logo Goes Here! */}
+          <img src={BeezerLogo} className='logo' />
+        </Link>
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
+            SHOP
+          </Link>
+          <Link className='nav-link' to='/sign-in'>
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
