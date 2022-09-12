958b49966828c5920c00a239d757f5059ebeb490
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-09-12 15:18:25 +0630

    Finish Creating User Documents

--- src/routes/signin/SignIn.jsx
+++ src/routes/signin/SignIn.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { createUserDocumentFromAuth, signInWithGooglePopup } from '../../utils/firebase/firebase';
+import { getRedirectResult } from 'firebase/auth';
+import React, { useEffect } from 'react'
+import {
+    auth,
+    createUserDocumentFromAuth,
+    signInWithGooglePopup,
+    signInWithGooleRedirect
+} from '../../utils/firebase/firebase';
 
 const SignIn = () => {
+
+    // useEffect( async () =>{
+    //     const response = await getRedirectResult(auth)
+    //     console.log(response)
+    // }, [])
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-      };
-  
+    };
+
+    //   const logGoogleRedirectUser = async () => {
+    //     const { user } = await signInWithGooleRedirect();
+    //     console.log(user)
+    //   };
+
     return (
-      <div>
-        <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-      </div>
+        <div>
+            <h1>Sign In Page</h1>
+            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+            <button onClick={signInWithGooleRedirect}>Sign in with Google Redirect</button>
+        </div>
     );
-  };
+};
 
 export default SignIn
