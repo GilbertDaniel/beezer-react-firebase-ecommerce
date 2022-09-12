811918b61b6114e4964f52637b607a3410f63afd
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-09-12 18:09:46 +0630

    Implementing SignIn

--- src/routes/signin/SignIn.jsx
+++ src/routes/signin/SignIn.jsx
@@ -1,38 +1,16 @@
-import { getRedirectResult } from 'firebase/auth';
-import React, { useEffect } from 'react'
+import SignInForm from '../../components/sign-in-form/SignInForm';
 import SignUpForm from '../../components/signup-form/SignUpForm';
-import {
-    auth,
-    createUserDocumentFromAuth,
-    signInWithGooglePopup,
-    signInWithGooleRedirect
-} from '../../utils/firebase/firebase';
+
+import './authentication.styles.scss';
 
 const SignIn = () => {
 
-    // useEffect( async () =>{
-    //     const response = await getRedirectResult(auth)
-    //     console.log(response)
-    // }, [])
-
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    };
-
-    //   const logGoogleRedirectUser = async () => {
-    //     const { user } = await signInWithGooleRedirect();
-    //     console.log(user)
-    //   };
-
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>Sign in with Google Popup</button>
-            {/* <button onClick={signInWithGooleRedirect}>Sign in with Google Redirect</button> */}
-            <SignUpForm/>
+        <div className='authentication-container'>
+          <SignInForm />
+          <SignUpForm />
         </div>
-    );
+      );
 };
 
 export default SignIn
