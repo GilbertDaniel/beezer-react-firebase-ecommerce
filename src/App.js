0262f865318c78111247a2f7717dcad3e81843c6
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-09-12 21:52:18 +0630

    Implementing Shop Page

--- src/App.js
+++ src/App.js
@@ -1,28 +1,16 @@
-import { useContext } from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes} from "react-router-dom";
 import Home from "./routes/home/Home";
 import Navigation from "./routes/navigation/Navigaton";
 import Authentication from "./routes/authentication/Authentication";
-import { UserContext } from './context/UserContext';
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from './routes/shop/Shop';
 
 const App = () => { 
-
-  const { currentUser } = useContext(UserContext);
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="shop" element={<Shop />} />
-        <Route
-          path="auth"
-          element={
-            currentUser ? <Navigate to="/" replace /> : <Authentication />
-          }
-        />
+        <Route path='shop' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
