f0f915da156824dab2c92b3fdcc991457afb5f32
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-09-19 20:59:37 +0630

    Implementing CategoryPage

--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -3,11 +3,13 @@ import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/CategoriesPreview';
 
 import './shop.styles.scss';
+import Category from './../category/Category';
 
 const Shop = () => {
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
+      <Route path=':category' element={<Category />} />
     </Routes>
   );
 };
