f37ef2d8893b48f9c7ee98e690740d2e017c46c6
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-09-19 21:32:06 +0630

    Adding Link

--- src/components/directory-item/DirectoryItem.jsx
+++ src/components/directory-item/DirectoryItem.jsx
@@ -1,21 +1,24 @@
+import { Link } from 'react-router-dom';
 import './directory-item.styles.scss';
 
 const DirectoryItem = ({ category }) => {
-  const { imageUrl, title } = category;
-  return (
-    <div className='directory-item-container'>
-      <div
-        className='background-image'
-        style={{
-          backgroundImage: `url(${imageUrl})`,
-        }}
-      />
-      <div className='body'>
-        <h2>{title}</h2>
-        <p>Shop Now</p>
-      </div>
-    </div>
-  );
+    const { imageUrl, title } = category;
+    return (
+        <div className='directory-item-container'>
+            <div
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+            />
+            <div className='body'>
+                <Link to={`/shop/${title}`}>
+                    <h2>{title}</h2>
+                    <p>Shop Now</p>
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export default DirectoryItem;
