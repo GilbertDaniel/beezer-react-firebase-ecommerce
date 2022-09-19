e541887968f53cce28be73b558f0e607fd9c7d4e
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-09-19 20:50:34 +0630

    Implementing Nested Routes in Shop

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import { Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/Home";
 import Navigation from "./routes/navigation/Navigaton";
 import Authentication from "./routes/authentication/Authentication";
 import Shop from './routes/shop/Shop';
 import Checkout from './routes/checkout/Checkout';
 
-const App = () => { 
+const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path='shop' element={<Shop />} />
+        <Route path='shop/*' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
         <Route path='checkout' element={<Checkout />} />
       </Route>
     </Routes>
   );
-  
+
 };
 
 export default App;
--- src/routes/shop/Shop.jsx
+++ src/routes/shop/Shop.jsx
@@ -1,18 +1,15 @@
-import React, {useContext} from 'react'
-import { CategoriesContext } from '../../context/CategoriesContext';
-import './shop.styles.scss';
-import CategoryPreview from './../../components/category-preview/CategoryPreview';
-const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
+import { Routes, Route } from 'react-router-dom';
 
+import CategoriesPreview from '../categories-preview/CategoriesPreview';
+
+import './shop.styles.scss';
+
+const Shop = () => {
   return (
-    <div className='shop-container'>
-      {Object.keys(categoriesMap).map((key) => {
-        const products = categoriesMap[key];
-        return <CategoryPreview key={key} title={key} products={products} />;
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 
-export default Shop
+export default Shop;
